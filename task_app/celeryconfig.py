eddd76f4df2f57084dc58ed7f20a678f29c31a65
Landry Breuil <breuil@craig.fr>
2024-08-06 09:41:34 +0200

    set worker_{task_,}log_format

--- task_app/celeryconfig.py
+++ task_app/celeryconfig.py
@@ -5,6 +5,10 @@ worker_concurrency = 1
 #autoscale = 8,1
 #imports = ('testcelery.tasks',)
 #worker_pool = solo
+worker_log_format = "[%(asctime)s: %(levelname)s/%(processName)s/%(threadName)s] WORKER %(message)s"
+worker_task_log_format = "[%(asctime)s: %(levelname)s/%(processName)s/%(threadName)s] TASK %(task_name)s[%(task_id)s]: %(message)s"
+
+
 # create the task entry in the backend when its started, not only when its finished
 task_track_started = True
 # debug print sql requests done to the backend
