938b88ff4c3bfba637730f25fd03c352c88379ed
Landry Breuil <breuil@craig.fr>
2024-08-09 09:53:52 +0200

    views: add /check/mapstore route queuing check_all_mapstore* tasks
    (variant depending on the subtasks parameter)

--- task_app/views.py
+++ task_app/views.py
@@ -8,7 +8,7 @@ from flask import request
 from flask import jsonify
 
 from . import tasks
-from task_app.checks.mapstore import check_res
+from task_app.checks.mapstore import check_res, check_all_mapstore_res, check_all_mapstore_res_subtasks
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
@@ -50,4 +50,13 @@ def check_map(mapid):
 @tasks_bp.route("/check/context/<int:ctxid>.json")
 def check_ctx(ctxid):
     result = check_res.delay('CONTEXT',ctxid)
+    return {"result_id": result.id}
+
+@tasks_bp.route("/check/mapstore", methods=['GET', 'POST'])
+def check_mapstore():
+    as_subtasks = request.args.get("subtasks", default=0, type=int)
+    if as_subtasks != 0:
+        result = check_all_mapstore_res.delay_subtasks()
+    else:
+        result = check_all_mapstore_res.delay()
     return {"result_id": result.id}
