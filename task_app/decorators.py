e49f321babd2340785a7f0a937607c10a1a16f9b
Landry Breuil <breuil@craig.fr>
2024-08-20 16:58:53 +0200

    [decorators] add decorator allowing to check for user role in req headers
    while here add a shortcut to check if the user has SUPERUSER

---
+++ task_app/decorators.py
@@ -0,0 +1,25 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 et
+from functools import wraps
+from flask import request, abort, jsonify
+
+def is_superuser():
+    roles = request.headers.get('sec-roles','').split(';')
+    return 'ROLE_SUPERUSER' in roles
+
+def check_role(role, json=False):
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            user_roles = request.headers.get('sec-roles','').split(';')
+            if 'ROLE_' + role not in user_roles:
+                if json:
+                    return jsonify({"message": "not authorized"}), 403
+                return abort(403)
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
