9a96605b7b17dc91add16df8c06dd6fe2b8e968c
Landry Breuil <breuil@craig.fr>
2024-10-29 08:26:51 +0100

    logger: we're in a flask context if request is defined

--- geordash/logwrap.py
+++ geordash/logwrap.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 # vim: ts=4 sw=4 et
 from celery.utils.log import get_task_logger
-from flask import current_app as app
-import sys
+from flask import request, current_app as app
 
 # wrapper to get the appropriate logger, depending if we're in a flask or celery context
 def get_logger(name=__name__):
-    if '/usr/bin/flask' in sys.argv:
+    if request:
         logger = app.logger
     else:
         logger = get_task_logger(__name__)
