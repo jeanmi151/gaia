50e0a5b50643338e27aa012f81ab37737acea9ab
Landry Breuil <breuil@craig.fr>
2024-10-30 13:50:26 +0100

    add gunicorn.conf.py to configure a gunicorn instance
    set the accesslog format to one not repeating the time (this information will
    be provided by journald anyway) and also log the right client/username from
    headers sent by the sec-proxy

---
+++ gunicorn.conf.py
@@ -0,0 +1,6 @@
+workers=4
+bind="0.0.0.0:5002"
+accesslog="-"
+access_log_format='%({x-forwarded-for}i)s %({sec-username}i)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+loglevel="info"
+wsgi_app="geordash:create_app()"
