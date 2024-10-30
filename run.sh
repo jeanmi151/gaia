7d9fcc278cd4550df960ad570443175dd85a670f
Landry Breuil <breuil@craig.fr>
2024-10-30 15:48:03 +0100

    run.sh: store celerybeat-schedule in current dir in dev mode
    workdir belongs to a different user

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . ./env
-celery -A $CELERY_APP worker --loglevel $CELERY_LOGLEVEL $CELERY_OPTS &
+celery -A $CELERY_APP worker --loglevel $CELERY_LOGLEVEL $CELERY_OPTS -s celerybeat-schedule &
 flask -A $FLASK_APP --debug run $FLASK_OPTS
