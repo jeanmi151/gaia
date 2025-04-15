f7d4b4df6bf3a176cec75f816bf64cd1c8c3454d
Landry Breuil <breuil@craig.fr>
2025-04-15 09:46:42 +0200

    script: render the check timestamp with a fr-FR locale

--- geordash/static/js/script.js
+++ geordash/static/js/script.js
@@ -370,7 +370,7 @@ const PollTaskRes = (type, resid, taskid, showdelete, targetdivid = '#pbtitle') 
                     $(targetpbdivid).empty();
                   }
                   const d = new Date(data["finished"] * 1000);
-                  $(targetpbdetdivid).text('vérification faite le '+ d);
+                  $(targetpbdetdivid).text('vérification faite le '+ d.toLocaleString("fr-FR"));
                   if ($(targetpreviousdivid).children().length == 0) {
                     $(targetpreviousdivid).html(ArrayToHtmlList([]));
                   }
