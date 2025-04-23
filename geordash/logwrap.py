cc00c7abef09c5758c8c67799dd3f1841924f272
Landry Breuil <breuil@craig.fr>
2025-04-23 13:34:38 +0200

    logwrap: rework get_logger to return the proper logger object
    be it in celery or flask context, and default to flask logger only
    if name is unset.

--- geordash/logwrap.py
+++ geordash/logwrap.py
@@ -3,11 +3,15 @@
 # vim: ts=4 sw=4 et
 from celery.utils.log import get_task_logger
 from flask import request, current_app as app
+import logging
 
 # wrapper to get the appropriate logger, depending if we're in a flask or celery context
 def get_logger(name=__name__):
     if request:
-        logger = app.logger
+        if name:
+            logger = logging.getLogger(name)
+        else:
+            logger = app.logger
     else:
-        logger = get_task_logger(__name__)
+        logger = get_task_logger(name)
     return logger
