eef28cb0051cf4dfbdb9235ce77ffab4e0505e71
Landry Breuil <breuil@craig.fr>
2025-04-25 09:44:32 +0200

    redisbackend: black

--- geordash/result_backend/redisbackend.py
+++ geordash/result_backend/redisbackend.py
@@ -4,17 +4,22 @@
 
 import redis
 import json
+
 try:
     from geordash.logwrap import get_logger
 except:
     # to run this module standalone for testing
     import logging
+
     logging.basicConfig(level=logging.DEBUG)
+
     def get_logger(name):
         return logging.getLogger(name)
 
+
 import sys
 from datetime import datetime, timezone
+
 
 class RedisClient:
     def __init__(self, url):
@@ -32,7 +37,7 @@ class RedisClient:
             except json.JSONDecodeError as e:
                 get_logger("RedisClient").error(f"discarding {k}, not json ? {e}")
                 continue
-            if 'children' in task and len(task['children']) > 0:
+            if "children" in task and len(task["children"]) > 0:
                 tsid = task["result"][0][0]
                 self.child_tasksets[tsid] = k.decode()[17:]
                 get_logger("RedisClient").debug(f"task {k} has a child taskset: {tsid}")
@@ -40,14 +45,20 @@ class RedisClient:
             args = task["args"]
             date_done = task["date_done"]
             if type(date_done) == str:
-                date_done = datetime.fromisoformat(date_done).replace(tzinfo=timezone.utc)
-            self.add_taskid_for_taskname_and_args(name, args, k.decode()[17:], date_done)
+                date_done = datetime.fromisoformat(date_done).replace(
+                    tzinfo=timezone.utc
+                )
+            self.add_taskid_for_taskname_and_args(
+                name, args, k.decode()[17:], date_done
+            )
 
         # analyse tasksets
         for k in self.r.scan_iter("celery-taskset-meta-*"):
             tsid = k.decode()[20:]
             if tsid in self.child_tasksets:
-                get_logger("RedisClient").debug(f"ignoring taskset with id {tsid}, already stored with task {self.child_tasksets[tsid]}")
+                get_logger("RedisClient").debug(
+                    f"ignoring taskset with id {tsid}, already stored with task {self.child_tasksets[tsid]}"
+                )
                 continue
             (name, args, date_done) = self.get_taskset_details(k.decode())
             self.add_taskid_for_taskname_and_args(name, args, tsid, date_done)
@@ -59,7 +70,9 @@ class RedisClient:
         """
         v = self.get(key)
         if v is None:
-            get_logger("RedisClient").error(f"found nothing for a taskset with {key}, shouldnt happen")
+            get_logger("RedisClient").error(
+                f"found nothing for a taskset with {key}, shouldnt happen"
+            )
             return (None, None, None)
         try:
             task = json.loads(v)
@@ -68,7 +81,7 @@ class RedisClient:
         name = None
         args = None
         date_done = None
-        for f in task['result'][1]:
+        for f in task["result"][1]:
             tid = f[0][0]
             tj = self.get(tid)
             if tj is None:
@@ -82,34 +95,44 @@ class RedisClient:
                 name = task["name"]
             else:
                 if task["name"] != name:
-                    get_logger("RedisClient").error(f"{name} mismatched task name for {tid}")
+                    get_logger("RedisClient").error(
+                        f"{name} mismatched task name for {tid}"
+                    )
             if args is None:
                 args = task["args"][:-1]
             # specialcase for check_resources job checking all maps *and* resources
-            if name.endswith('check_res') and args is not None and task["args"][:-1] != args:
+            if (
+                name.endswith("check_res")
+                and args is not None
+                and task["args"][:-1] != args
+            ):
                 args = []
             # find the last finishing job
             if type(task["date_done"]) == str:
-                subtask_done = datetime.fromisoformat(task["date_done"]).replace(tzinfo=timezone.utc)
+                subtask_done = datetime.fromisoformat(task["date_done"]).replace(
+                    tzinfo=timezone.utc
+                )
             else:
                 subtask_done = task["date_done"]
-                get_logger("RedisClient").debug(f"sd={subtask_done}, type={type(subtask_done)}")
+                get_logger("RedisClient").debug(
+                    f"sd={subtask_done}, type={type(subtask_done)}"
+                )
             if date_done is None:
                 date_done = subtask_done
             elif subtask_done is not None and subtask_done > date_done:
                 date_done = subtask_done
             # print(f"{tid} {task['name']} {task['args'][:-1]} {task['date_done']} {date_done}")
         if name is not None:
-            if name.endswith('owslayer'):
-                name = 'geordash.checks.ows.owsservice'
-            if name.endswith('check_res'):
-                name = 'geordash.checks.mapstore.check_resources'
-            if name.endswith('check_record'):
-                name = 'geordash.checks.csw.check_catalog'
+            if name.endswith("owslayer"):
+                name = "geordash.checks.ows.owsservice"
+            if name.endswith("check_res"):
+                name = "geordash.checks.mapstore.check_resources"
+            if name.endswith("check_record"):
+                name = "geordash.checks.csw.check_catalog"
         return (name, args, date_done)
 
     def get(self, key):
-#        print(f"get({key}) called")
+        #        print(f"get({key}) called")
         if key[:17] == "celery-task-meta-" or key[:20] == "celery-taskset-meta-":
             return self.r.get(key)
         else:
@@ -135,12 +158,12 @@ class RedisClient:
         task = json.loads(v)
         taskname = None
         args = None
-        if 'name' in task:
+        if "name" in task:
             taskname = task["name"]
             args = task["args"]
         else:
             # taskset, take name & args from the first task of the set
-            ftid = task['result'][1][0][0][0]
+            ftid = task["result"][1][0][0][0]
             tj = self.get(ftid)
             try:
                 task = json.loads(tj)
@@ -151,13 +174,13 @@ class RedisClient:
                 get_logger("RedisClient").error(f"discarding {ftid}, {str(e)}")
                 return None
             args = task["args"][:-1]
-            if task["name"].endswith('owslayer'):
-                taskname = 'geordash.checks.ows.owsservice'
-            if task["name"].endswith('check_res'):
-                taskname = 'geordash.checks.mapstore.check_resources'
+            if task["name"].endswith("owslayer"):
+                taskname = "geordash.checks.ows.owsservice"
+            if task["name"].endswith("check_res"):
+                taskname = "geordash.checks.mapstore.check_resources"
                 args = []
-            if task["name"].endswith('check_record'):
-                taskname = 'geordash.checks.csw.check_catalog'
+            if task["name"].endswith("check_record"):
+                taskname = "geordash.checks.csw.check_catalog"
 
         if taskname in self.task_by_taskname:
             if tuple(args) in self.task_by_taskname[taskname]:
@@ -186,16 +209,25 @@ class RedisClient:
                     if taskids[taskid]["finished"] is None:
                         taskb = json.loads(v)
                         date_done = None
-                        if 'date_done' in taskb and taskb["date_done"] is not None:
+                        if "date_done" in taskb and taskb["date_done"] is not None:
                             if type(taskb["date_done"]) == str:
-                                date_done = datetime.fromisoformat(taskb["date_done"]).replace(tzinfo=timezone.utc)
+                                date_done = datetime.fromisoformat(
+                                    taskb["date_done"]
+                                ).replace(tzinfo=timezone.utc)
                             else:
                                 date_done = taskb["date_done"]
-                        if not 'name' in taskb: # taskset
-                            (x, y, date_done) = self.get_taskset_details("celery-taskset-meta-" + taskid)
+                        if not "name" in taskb:  # taskset
+                            (x, y, date_done) = self.get_taskset_details(
+                                "celery-taskset-meta-" + taskid
+                            )
                         if date_done is not None:
-                            taskids[taskid] = { "finished": date_done }
-                    found_taskids.append({'id': taskid, 'finished': taskids[taskid]["finished"].timestamp() })
+                            taskids[taskid] = {"finished": date_done}
+                    found_taskids.append(
+                        {
+                            "id": taskid,
+                            "finished": taskids[taskid]["finished"].timestamp(),
+                        }
+                    )
 
                 # remove taskids from the in-memory list
                 for t in dropped_taskids:
@@ -206,43 +238,59 @@ class RedisClient:
     def add_taskid_for_taskname_and_args(self, taskname, args, taskid, finished=None):
         if taskname not in self.task_by_taskname:
             self.task_by_taskname[taskname] = dict()
-        if args is None: # invalid task ?
+        if args is None:  # invalid task ?
             return
         if tuple(args) not in self.task_by_taskname[taskname]:
             self.task_by_taskname[taskname][tuple(args)] = dict()
-        self.task_by_taskname[taskname][tuple(args)][taskid] = {'finished': finished}
+        self.task_by_taskname[taskname][tuple(args)][taskid] = {"finished": finished}
 
     def get_last_taskid_for_taskname_and_args(self, taskname, args):
         tasks = self.get_taskids_by_taskname_and_args(taskname, args)
 
         # lambda function for max() call below
         def compare_task_by_finished_ts(o):
-            if isinstance(o['finished'], datetime):
-                return str(o['finished'])
-            return o['finished']
+            if isinstance(o["finished"], datetime):
+                return str(o["finished"])
+            return o["finished"]
 
         if tasks:
             last_task = max(tasks, key=compare_task_by_finished_ts)
-            return last_task['id']
+            return last_task["id"]
         return None
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import sys
-    sys.path.append('.')
+
+    sys.path.append(".")
     import config
+
     rc = RedisClient(config.url)
     print(rc.task_by_taskname["geordash.checks.mapstore.check_res"].keys())
     print(rc.task_by_taskname["geordash.checks.mapstore.check_res"][("CONTEXT", 38)])
-    print(tuple(["CONTEXT", 38]) in rc.task_by_taskname["geordash.checks.mapstore.check_res"])
+    print(
+        tuple(["CONTEXT", 38])
+        in rc.task_by_taskname["geordash.checks.mapstore.check_res"]
+    )
     print(len(rc.get("celery-task-meta-c2bd9571-0ff0-4272-b882-f813c35a2d44")))
     print(len(rc.get("c2bd9571-0ff0-4272-b882-f813c35a2d44")))
     print(len(rc.get("celery-taskset-meta-9678631a-7fb6-42e0-83a9-1b9a9558949f")))
-    tasks = rc.get_taskids_by_taskname_and_args("geordash.checks.mapstore.check_res", ["MAP", 1])
+    tasks = rc.get_taskids_by_taskname_and_args(
+        "geordash.checks.mapstore.check_res", ["MAP", 1]
+    )
     print(tasks)
     for f in tasks:
-        print(len(rc.get(f['id'])))
-        print(f['finished'])
-        j = json.loads(rc.get(f['id']))
+        print(len(rc.get(f["id"])))
+        print(f["finished"])
+        j = json.loads(rc.get(f["id"]))
         print(j["result"]["problems"])
-    print(rc.get_taskids_by_taskname_and_args("geordash.checks.ows.owsservice", ["wfs", "/wxs/geor_loc/ows"]))
-    print(rc.get_last_taskid_for_taskname_and_args("geordash.checks.ows.owsservice", ["wfs", "/wxs/geor_loc/ows"]))
+    print(
+        rc.get_taskids_by_taskname_and_args(
+            "geordash.checks.ows.owsservice", ["wfs", "/wxs/geor_loc/ows"]
+        )
+    )
+    print(
+        rc.get_last_taskid_for_taskname_and_args(
+            "geordash.checks.ows.owsservice", ["wfs", "/wxs/geor_loc/ows"]
+        )
+    )
