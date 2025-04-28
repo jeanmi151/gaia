33277c6727233e91b29cf1b603fb1e02aa968ca3
Landry Breuil <breuil@craig.fr>
2025-04-28 14:56:45 +0200

    script: add a FetchCswRecords() function (#10)
    calls the fetchcswrecords/<portal>.json route added in ca60f7ea2e8 to trigger a
    background csw GetRecords, and then periodically polls fetchcswresults/<taskid>
    until caching is done (or failed).
    
    when the caching succeded, reload the current page.

--- geordash/static/js/script.js
+++ geordash/static/js/script.js
@@ -253,6 +253,38 @@ const DeleteTask = (taskid) => {
     });
 }
 
+const FetchCswRecords = (portal) => {
+  const targetdivid = '#progress';
+  fetch(baseurl + '/tasks/fetchcswrecords/' + portal + '.json')
+    .then(response => response.json())
+    .then(mydata => {
+      $(targetdivid).text("Queuing background task..");
+      const poll = () => {
+        fetch(baseurl + '/tasks/fetchcswresults/' + mydata["taskid"])
+          .then(response => response.json())
+          .then(data => {
+//            console.log(data)
+            if (data === null) {
+              $(targetdivid).text('got null, shouldnt happen ?');
+            } else if (!data["state"] || data["state"] == 'FAILURE') {
+              $(targetdivid).text("Protch ! Check browser console");
+              console.error(data)
+            } else if(data["state"] == 'PENDING' || data["state"] == 'STARTED') {
+              $(targetdivid).text("waiting..");
+              setTimeout(poll, 1000)
+            } else if(data["state"] == 'PROGRESS') {
+              $(targetdivid).text("fetched " + data["completed"]["current"] + ' over ' + data["completed"]["total"] + ' potential records');
+              setTimeout(poll, 1000)
+            } else if(data["state"] == 'SUCCESS') {
+              $(targetdivid).text("successfully fetched " + data["completed"] + " records, reloading page");
+              window.location.reload();
+            }
+          })
+      }
+      poll();
+    });
+}
+
 const CheckRes = (type, resid, showdelete, targetdivid = '#pbtitle') => {
   fetch(baseurl + '/tasks/check/' + type + '/' + resid + '.json')
     .then(response => response.json())
