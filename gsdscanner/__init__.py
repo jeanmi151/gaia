2ff87cabe8e72036ae3f2d32f1dc6258f5826905
Landry Breuil <breuil@craig.fr>
2025-06-11 11:04:53 +0200

    gsdscanner: rework parse/parseAll to allow progressive parsing
    if called with a list, parse() calls itself on each element of the list, so
    that parseAll still parses all the collections.  allows an upcoming async
    parse_gsd task to progressively parse everything.
    
    black all the things while here.

--- gsdscanner/__init__.py
+++ gsdscanner/__init__.py
@@ -18,24 +18,99 @@ from .vectordata import VectorData
 from .rasterdata import RasterData
 from .collection import Collection
 
+
 class GSDatadirScanner:
-    def __init__(self, path='/srv/data/geoserver'): # default path for ansible deployments
+    def __init__(
+        self, path="/srv/data/geoserver"
+    ):  # default path for ansible deployments
         self.basepath = path
         self.collections = dict()
+        self.available_keys = [
+            "workspaces",
+            "datastores",
+            "coveragestores",
+            "namespaces",
+            "featuretypes",
+            "coverages",
+            "layers",
+            "styles",
+            "slds",
+            "vectordatas",
+            "rasterdatas",
+        ]
         # XXX handle access errors re ownership/rights
-        tree = etree.parse(f'{path}/global.xml')
-        self.pburl = getelemat(tree, '/global/settings/proxyBaseUrl')
-        self.version = getelemat(tree, '/global/updateSequence')
+        tree = etree.parse(f"{path}/global.xml")
+        self.pburl = getelemat(tree, "/global/settings/proxyBaseUrl")
+        self.version = getelemat(tree, "/global/updateSequence")
+
+    def parse(self, toparse):
+        print(f"parse({toparse})")
+        if type(toparse) == list:
+            for i in toparse:
+                self.parse(i)
+        else:
+            match toparse:
+                case "workspaces":
+                    self.collections["workspaces"] = Collection(
+                        f"{self.basepath}/workspaces/*/workspace.xml", Workspace
+                    )
+                case "datastores":
+                    self.collections["datastores"] = Collection(
+                        f"{self.basepath}/workspaces/*/*/datastore.xml", Datastore
+                    )
+                case "coveragestores":
+                    self.collections["coveragestores"] = Collection(
+                        f"{self.basepath}/workspaces/*/*/coveragestore.xml",
+                        Coveragestore,
+                    )
+                case "namespaces":
+                    self.collections["namespaces"] = Collection(
+                        f"{self.basepath}/workspaces/*/namespace.xml", Namespace
+                    )
+                case "featuretypes":
+                    self.collections["featuretypes"] = Collection(
+                        f"{self.basepath}/workspaces/*/*/*/featuretype.xml", FeatureType
+                    )
+                case "coverages":
+                    self.collections["coverages"] = Collection(
+                        f"{self.basepath}/workspaces/*/*/*/coverage.xml", Coverage
+                    )
+                case "layers":
+                    self.collections["layers"] = Collection(
+                        f"{self.basepath}/workspaces/*/*/*/layer.xml", Layer
+                    )
+                case "styles":
+                    self.collections["styles"] = Collection(
+                        [
+                            f"{self.basepath}/styles/*.xml",
+                            f"{self.basepath}/workspaces/*/styles/*.xml",
+                        ],
+                        Style,
+                    )
+                case "slds":
+                    self.collections["slds"] = Collection(
+                        [
+                            f"{self.basepath}/styles/*.sld",
+                            f"{self.basepath}/workspaces/*/styles/*.sld",
+                        ],
+                        SLD,
+                    )
+                case "vectordatas":
+                    self.collections["vectordatas"] = Collection(
+                        [
+                            f"{self.basepath}/data/**/{f}"
+                            for f in ["*.shp", "*.SHP", "*.gpkg"]
+                        ],
+                        VectorData,
+                    )
+                case "rasterdatas":
+                    self.collections["rasterdatas"] = Collection(
+                        [f"{self.basepath}/data/**/{f}" for f in ["*.tif", "*.tiff"]],
+                        RasterData,
+                    )
+                case _:
+                    pass
+                    ## XXX unreachable
 
     def parseAll(self):
-        self.collections['workspaces'] = Collection(f"{self.basepath}/workspaces/*/workspace.xml", Workspace)
-        self.collections['datastores'] = Collection(f"{self.basepath}/workspaces/*/*/datastore.xml", Datastore)
-        self.collections['coveragestores'] = Collection(f"{self.basepath}/workspaces/*/*/coveragestore.xml", Coveragestore)
-        self.collections['namespaces'] = Collection(f"{self.basepath}/workspaces/*/namespace.xml", Namespace)
-        self.collections['featuretypes'] = Collection(f"{self.basepath}/workspaces/*/*/*/featuretype.xml", FeatureType)
-        self.collections['coverages'] = Collection(f"{self.basepath}/workspaces/*/*/*/coverage.xml", Coverage)
-        self.collections['layers'] = Collection(f"{self.basepath}/workspaces/*/*/*/layer.xml", Layer)
-        self.collections['styles'] = Collection([f"{self.basepath}/styles/*.xml", f"{self.basepath}/workspaces/*/styles/*.xml"], Style)
-        self.collections['slds'] = Collection([f"{self.basepath}/styles/*.sld", f"{self.basepath}/workspaces/*/styles/*.sld"], SLD)
-        self.collections['vectordatas'] = Collection([f"{self.basepath}/data/**/{f}" for f in ['*.shp', '*.SHP', '*.gpkg']], VectorData)
-        self.collections['rasterdatas'] = Collection([f"{self.basepath}/data/**/{f}" for f in ['*.tif', '*.tiff']], RasterData)
+        self.parse(self.available_keys)
