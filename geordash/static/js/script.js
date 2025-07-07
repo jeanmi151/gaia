f16d34af30ba16b57dd4680e77ff6960d788b293
Landry Breuil <breuil@craig.fr>
2025-07-07 11:00:41 +0200

    PollTaskRes: job arg is a real url, so replace / by ~ in xurl
    fixes links to per-config page in the 'all config problems' table.
    improves on 427a7d6

--- geordash/static/js/script.js
+++ geordash/static/js/script.js
@@ -335,7 +335,7 @@ const PollTaskRes = (type, resid, taskid, showdelete, targetdivid = '#pbtitle') 
                               }
                               return {'url': `${j.args[0]} ${j.args[1]}`, 'xurl': xurl, 'problem': GetPbStr(i) }
                             } else if (data['task'].includes('mviewer.check_all')) {
-                              return {'url': j.args[0].replaceAll('~','/'), 'xurl': baseurl + '/mviewer/' + j.args[0], 'problem': GetPbStr(i) }
+                              return {'url': j.args[0], 'xurl': baseurl + '/mviewer/' + j.args[0].replaceAll('/','~'), 'problem': GetPbStr(i) }
                             } else {
                               // csw
                               if (data['task'].includes('csw') && j.args.length == 2) {
