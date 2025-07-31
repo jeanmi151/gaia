a55ad9124ae214c0756fd70fa34af89320f7a1ce
Landry Breuil <breuil@craig.fr>
2025-07-31 10:20:08 +0200

    checkgsd: rewrite to use gsdatadir_item check
    most of the asserts that were there have been moved to proper
    tests with problem reporting in checks/gsd.py

--- checkgsd.py
+++ checkgsd.py
@@ -5,57 +5,16 @@
 from os.path import dirname
 from gsdscanner import GSDatadirScanner
 from geordash.utils import find_geoserver_datadir
+from geordash.checks.gsd import gsdatadir_item
 
 gds = GSDatadirScanner(find_geoserver_datadir('/data/webapps/geoserver'))
 print(f"datadir version: {gds.version}")
 gds.parseAll()
-for d in ('workspaces', 'datastores', 'namespaces', 'featuretypes', 'layers', 'styles', 'slds', 'vectordatas', 'rasterdatas'):
-    for e in gds.collections[d].coll:
-        print(f"{e} -> {gds.collections[d].coll[e]}")
-
-for e in gds.collections['datastores'].coll:
-    ds = gds.collections['datastores'].coll[e]
-    assert gds.collections['workspaces'].has(ds.workspaceid)
-print(f"checked {len(gds.collections['datastores'].coll)} datastores")
-
-for e in gds.collections['coveragestores'].coll:
-    cs = gds.collections['coveragestores'].coll[e]
-    assert gds.collections['workspaces'].has(cs.workspaceid)
-print(f"checked {len(gds.collections['coveragestores'].coll)} coveragestores")
-
-for e in gds.collections['featuretypes'].coll:
-    ft = gds.collections['featuretypes'].coll[e]
-    assert gds.collections['datastores'].has(ft.datastoreid)
-    assert gds.collections['namespaces'].has(ft.namespaceid)
-    # if datastore is of type JNDI, look for a table name matching nativeName in the linked postgis JNDI cnx
-
-print(f"checked {len(gds.collections['featuretypes'].coll)} featuretypes")
-
-for e in gds.collections['layers'].coll:
-    l = gds.collections['layers'].coll[e]
-    if l.featuretypeid.startswith('FeatureTypeInfoImpl'):
-        assert gds.collections['featuretypes'].has(l.featuretypeid)
-    elif l.featuretypeid.startswith('CoverageInfoImpl'):
-        assert gds.collections['coverages'].has(l.featuretypeid)
-print(f"checked {len(gds.collections['layers'].coll)} layers")
-
-for e in gds.collections['slds'].coll:
-    s = gds.collections['slds'].coll[e]
-    if s.filesize <= 0:
-        print(f"{s.file} is empty")
-print(f"checked {len(gds.collections['slds'].coll)} slds")
-
-for e in gds.collections['styles'].coll:
-    s = gds.collections['styles'].coll[e]
-    if '/workspaces/' in s.file:
-        assert gds.collections['workspaces'].has(s.workspaceid)
-    else:
-        assert s.workspaceid == None
-    # check that sldfilename relative to style exists XXX doesnt handle css styles yet
-    sldpath = dirname(s.file) + '/' + s.sldfilename
-    if not gds.collections['slds'].has(sldpath) and not sldpath.endswith('.css'):
-        print(f"{sldpath} not found")
-print(f"checked {len(gds.collections['styles'].coll)} styles")
+for colltype in gds.available_keys:
+    for k in gds.collections[colltype].coll:
+        ret = gsdatadir_item(colltype, k, gsd=gds)
+        if len(ret['problems']) > 0:
+            print(f"{colltype} {k} -> {ret['problems']}")
 
 # check for RasterData/VectorData
 # for Vector:
