3aef9783177fec42eb79f59239a43c9118732449
Landry Breuil <breuil@craig.fr>
2025-08-05 14:55:42 +0200

    checkgsd: drop now useless comment

--- checkgsd.py
+++ checkgsd.py
@@ -15,18 +15,3 @@ for colltype in gds.available_keys:
         ret = gsdatadir_item(colltype, k, gsd=gds)
         if len(ret['problems']) > 0:
             print(f"{colltype} {k} -> {ret['problems']}")
-
-# check for RasterData/VectorData
-# for Vector:
-# if shapefile:
-#   look for a type=ShapeFile datastore pointing at the parent folder
-#   or a 'directory of spatial files (shapefiles) datastore pointing at the parent folder (connectionParameters, entry key=url)
-#   then featuretype.xml/nativeName=shapefile name in folder ?
-# if *.gpkg:
-#  look for a type=GeoPackage datastore with connectionParameters/entry[name=database] pointing at the geopackage file
-#   then featuretype.xml/nativeName=layer name in the geopackage
-# for Raster:
-# look for a CoverageStore type=GeoTIFF pointing at the relative path
-# or a CoverageStore type=ImageMosaic pointing at the parent folder, folder should have a VectorData/tileindex with a feature pointing a the raster
-
-# check that all layers appear in a wms getcap
